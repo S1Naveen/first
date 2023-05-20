a9a4937d330258a56b6edb9c3b9179e37fb30a9e
S1Naveen <naveen082529@gmail.com>
2023-05-20 13:03:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def srujana(request):
+    return HttpResponse('<h1>he:em kura?<br/> she:kakarakay!!<br/> he:chedu ga leda?<br/> she:ma amma chekkara vesindi...</h1>')
